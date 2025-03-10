780d7f994b6692d0aed054fe5d497d5f70c69a6e
Zayan Mustafa <mustafazayyan13@gmail.com>
2025-03-10 17:57:31 +0500

    Navbar Complete

---
+++ src/app/component/navbar.js
@@ -0,0 +1,47 @@
+"use client"
+
+import Link from "next/link"
+import { NAVBAR_LINKS } from "../constant/navlink"
+import { useState } from "react"
+// import { Menu } from "lucide-react"
+import { IoMdMenu } from "react-icons/io";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  return (
+    <header className="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md">
+      <div className="flex-1 flex justify-between items-center">
+        <Link href="/home" className="text-xl">
+          Madni Courses Korangi Town
+        </Link>
+      </div>
+
+      <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block" onClick={toggleMenu}>
+        <IoMdMenu className="text-gray-900 w-5 h-5" />
+      </label>
+      <input className="hidden" type="checkbox" id="menu-toggle" checked={isMenuOpen} readOnly />
+
+      <div className={`${isMenuOpen ? "block" : "hidden"} md:flex md:items-center md:w-auto w-full`} id="menu">
+        <nav>
+          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+            {NAVBAR_LINKS.map((link, index) => (
+              <li key={index}>
+                <Link className="md:p-4 py-3 px-0 block hover:text-indigo-400 flex items-center" href={link.path}>
+                  <span className="ml-2">{link.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
+
---
+++ src/app/constant/navlink.js
@@ -0,0 +1,9 @@
+// constant/navlink.js
+import { FaHome, FaInfoCircle, FaCogs, FaEnvelope } from 'react-icons/fa';
+
+export const NAVBAR_LINKS = [
+  { name: 'AboutUs', path: '/about',  },
+  { name: 'Treatments', path: '/treatments', },
+  { name: 'Blog', path: '/blog',  },
+  { name: 'Contact Us', path: '/contact', },
+];
